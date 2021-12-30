b749040ef46da02220355fb1c9e2158f79a39d8c
fuk_u_pikachu <hieu24901@gmail.com>
2021-12-30 16:15:08 +0700

    ConnectDB

--- DATH#02/GUI_Con_Cung_App/GUI_Con_Cung_App/Program.cs
+++ DATH#02/GUI_Con_Cung_App/GUI_Con_Cung_App/Program.cs
@@ -1,19 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GUI_Con_Cung_App
 {
-    static class Connection_string
-    {
-        public static string Conn_String()
-        {
-            return "Data Source=LAPTOP-FMCUOBFE\\SQLEXPRESS;Initial Catalog=DB_QLDatChuyenHang;Integrated Security=True";
-        }   
-    }
-    static class Program
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -23,7 +17,7 @@ namespace GUI_Con_Cung_App
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new HomePage());
+            Application.Run(new ConnectDB()); // go to ConnectDB.cs, F7 view code
 
         }
     }
