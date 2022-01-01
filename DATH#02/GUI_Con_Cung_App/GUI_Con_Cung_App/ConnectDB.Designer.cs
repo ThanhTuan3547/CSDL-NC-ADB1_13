2dc2acf630493c63ba13ec473b119941e72b4a1e
fuk_u_pikachu <hieu24901@gmail.com>
2022-01-01 22:42:56 +0700

    push

--- DATH#02/GUI_Con_Cung_App/GUI_Con_Cung_App/ConnectDB.Designer.cs
+++ DATH#02/GUI_Con_Cung_App/GUI_Con_Cung_App/ConnectDB.Designer.cs
@@ -39,14 +39,14 @@ namespace GUI_Con_Cung_App
             // 
             // Server_textBox
             // 
-            this.Server_textBox.Location = new System.Drawing.Point(83, 72);
+            this.Server_textBox.Location = new System.Drawing.Point(82, 64);
             this.Server_textBox.Name = "Server_textBox";
             this.Server_textBox.Size = new System.Drawing.Size(164, 20);
             this.Server_textBox.TabIndex = 0;
             // 
             // Database_textBox
             // 
-            this.Database_textBox.Location = new System.Drawing.Point(83, 98);
+            this.Database_textBox.Location = new System.Drawing.Point(82, 90);
             this.Database_textBox.Name = "Database_textBox";
             this.Database_textBox.Size = new System.Drawing.Size(164, 20);
             this.Database_textBox.TabIndex = 1;
@@ -54,7 +54,7 @@ namespace GUI_Con_Cung_App
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 75);
+            this.label1.Location = new System.Drawing.Point(12, 67);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(51, 13);
             this.label1.TabIndex = 2;
@@ -64,7 +64,7 @@ namespace GUI_Con_Cung_App
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 101);
+            this.label2.Location = new System.Drawing.Point(12, 93);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 3;
@@ -72,7 +72,7 @@ namespace GUI_Con_Cung_App
             // 
             // Connect_Button
             // 
-            this.Connect_Button.Location = new System.Drawing.Point(253, 72);
+            this.Connect_Button.Location = new System.Drawing.Point(252, 64);
             this.Connect_Button.Name = "Connect_Button";
             this.Connect_Button.Size = new System.Drawing.Size(70, 46);
             this.Connect_Button.TabIndex = 4;
@@ -84,7 +84,7 @@ namespace GUI_Con_Cung_App
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Good Times Rg", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(47, 28);
+            this.label3.Location = new System.Drawing.Point(60, 23);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(230, 19);
             this.label3.TabIndex = 5;
@@ -95,7 +95,7 @@ namespace GUI_Con_Cung_App
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(352, 159);
+            this.ClientSize = new System.Drawing.Size(352, 137);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.Connect_Button);
             this.Controls.Add(this.label2);
